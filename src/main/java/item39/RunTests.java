eacd1735775226132c8018485981a15de65e22b4
doyoun8813 <doyoun8813@gmail.com>
2023-05-29 04:29:42 +0900

    ver2

--- src/main/java/item39/RunTests.java
+++ src/main/java/item39/RunTests.java
@@ -9,24 +9,27 @@ public class RunTests {
 		
 		int tests = 0;
 		int passed = 0;
-		Class<?> testClass = Class.forName("item39.Sample");
+		Class<?> testClass = Class.forName("item39.Sample2");
 		for (Method m : testClass.getDeclaredMethods()) {
-			if(m.isAnnotationPresent(Test.class)) {
+			if(m.isAnnotationPresent(ExceptionTest.class)) {
 				tests++;
 				try {
 					m.invoke(null);
-					passed++;
+					System.out.printf("테스트 %s 실패: 예외를 던지지 않음%n", m);
 				} catch (InvocationTargetException wrappedExc) {
 					Throwable exc = wrappedExc.getCause();
-					System.out.println(m + " 실패:" + exc);
+					Class<? extends Throwable> excType = 
+							m.getAnnotation(ExceptionTest.class).value();
+					if(excType.isInstance(exc)) {
+						passed++;
+					}else {
+						System.out.printf("테스트 %s 실패: 기대한 예외 %s,\n 발생한 예외 %s%n", m, excType.getName(), exc);					
+					}
 				} catch (Exception exc) {
-					System.out.println("잘못 사용한 @Test: " + m);
+					System.out.println("잘못 사용한 @ExceptionTest: " + m);
 				}
 			}
 		}
-		
-		System.out.printf("성공: %d, 실패: %d%n", passed, tests - passed);
-		
+		System.out.printf("성공: %d, 실패: %d%n", passed, tests - passed);		
 	}
-
 }
