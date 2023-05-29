0df21ea555f194689a2ae3fa13bc9a86d393cf09
doyoun8813 <doyoun8813@gmail.com>
2023-05-30 01:14:49 +0900

    item41

--- src/main/java/item40/Bigram.java
+++ src/main/java/item40/Bigram.java
@@ -14,8 +14,12 @@ public class Bigram {
 		this.second = second;
 	}
 	
-	
-	public boolean equals(Bigram b) {
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof Bigram)) {
+			return false;
+		}
+		Bigram b = (Bigram) o;
 		return b.first == first && b.second == second;
 	}
 	
