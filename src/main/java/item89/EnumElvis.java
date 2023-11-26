172538f03d98913fcbd96247f43bad3d3a3a6247
doyoun8813 <doyoun8813@gmail.com>
2023-11-27 01:05:01 +0900

    Item89.인스턴스 수를 통제해야 한다면 readResolver보다는 열거 타입을 사용하라

---
+++ src/main/java/item89/EnumElvis.java
@@ -0,0 +1,12 @@
+package item89;
+
+import java.util.Arrays;
+
+public enum EnumElvis {
+    INSTANCE;
+    private String[] favoriteSongs =
+            {"Hound Dog", "Heartbreak Hotel"};
+    public void printFavorites(){
+        System.out.println(Arrays.toString(favoriteSongs));
+    }
+}
